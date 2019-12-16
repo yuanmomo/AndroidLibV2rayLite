a203a24e5666785e32b2cae9971a9c9a5c06b1b2
aaaaa <aaaa@gmail.com>
2019-12-16 16:29:45 +0800

    update build on linux shell

--- build/build-on-linux.sh
+++ build/build-on-linux.sh
@@ -2,50 +2,8 @@
 
 set -e
 
-current_dir=$(cd "$(dirname "$0")";pwd)
-root_dir=${current_dir}/../
-
-# ------------------------------------------------------
-# --- set ENV
-DOWNLOAD_URL="https://dl.google.com/android/repository/"
-SDK_FILE_NAME="sdk-tools-linux-4333796.zip"
-GO_FILE_NAME="go1.13.5.linux-amd64.tar.gz"
-
-ANDROID_HOME="/usr/local/android-sdk"
-ANDROID_NDK_HOME="${ANDROID_HOME}/ndk-bundle"
-PACKAGE_INSTALL_FILE="android_package_to_install"
-
-#cd "${root_dir}" && git pull
-
-# install docker first
-if [[ ! $(command -v docker) ]]; then
-    echo "自动安装 docker 。。。。。"
-    bash <(curl -s -L get.docker.com)
-    service docker restart
-fi
-
-
-# ------------------------------------------------------
-# --- Download GO into /usr/local/go and set go ENV
-# IMPORTANT:  gomobile not support go modules !!!!
-if [[ ! $(command -v go) ]]; then
-    wget -q https://dl.google.com/go/${GO_FILE_NAME} && tar -C /usr/local -xzf ${GO_FILE_NAME} && rm -f ${GO_FILE_NAME}*
-
-    # update PATH
-    echo 'export PATH=${PATH}'":/usr/local/go/bin:/root/go/bin" >> ~/.bashrc
-    source ~/.bashrc
-fi
-
-# ------------------------------------------------------
-# --- Download Android SDK tools into $ANDROID_HOME
-if [[ ! $(command -v sdkmanager) ]]; then
-    wget -q ${DOWNLOAD_URL}/${SDK_FILE_NAME} && unzip ${SDK_FILE_NAME} -d ${ANDROID_HOME} && rm -rf ${SDK_FILE_NAME}*
-
-    # update PATH
-    echo 'export PATH=${PATH}'":${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools" >> ~/.bashrc
-    source ~/.bashrc
-fi
-
+# param check
+echo "check params ...."
 cmdname=$(basename $0)
 useage(){
     cat << USAGE  >&2
@@ -82,9 +40,60 @@ for param in "$@"; do
   esac
 done
 
+
+echo "set env ......"
+current_dir=$(cd "$(dirname "$0")";pwd)
+root_dir=${current_dir}/../
+
+# ------------------------------------------------------
+# --- set ENV
+DOWNLOAD_URL="https://dl.google.com/android/repository/"
+SDK_FILE_NAME="sdk-tools-linux-4333796.zip"
+GO_FILE_NAME="go1.13.5.linux-amd64.tar.gz"
+
+ANDROID_HOME="/usr/local/android-sdk"
+ANDROID_NDK_HOME="${ANDROID_HOME}/ndk-bundle"
+PACKAGE_INSTALL_FILE="android_package_to_install"
+
+#cd "${root_dir}" && git pull
+
+# install docker first
+if [[ ! $(command -v docker) ]]; then
+    echo "install docker 。。。。。"
+    bash <(curl -s -L get.docker.com)
+    service docker restart
+fi
+
+
+# ------------------------------------------------------
+# --- Download GO into /usr/local/go and set go ENV
+# IMPORTANT:  gomobile not support go modules !!!!
+if [[ ! $(command -v go) ]]; then
+    echo "install go  ....."
+    wget -q https://dl.google.com/go/${GO_FILE_NAME} && tar -C /usr/local -xzf ${GO_FILE_NAME} && rm -f ${GO_FILE_NAME}*
+
+    # update PATH
+    echo 'export PATH=${PATH}'":/usr/local/go/bin:/root/go/bin" >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+# ------------------------------------------------------
+# --- Download Android SDK tools into $ANDROID_HOME
+if [[ ! $(command -v sdkmanager) ]]; then
+    echo "install android sdk and sdkmanager ....."
+    wget -q ${DOWNLOAD_URL}/${SDK_FILE_NAME} && unzip ${SDK_FILE_NAME} -d ${ANDROID_HOME} && rm -rf ${SDK_FILE_NAME}*
+
+    # update PATH
+    echo 'export PATH=${PATH}'":${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools" >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+
 if [[ ${update_android_sdk} == "1" ]] ; then
     # ------------------------------------------------------
     # --- install package and NDK
+    echo "sdkmanager install tools ....."
+
     sdkmanager --verbose --list | awk -f ${root_dir}/build/awk/parse.awk > ${ANDROID_HOME}/${PACKAGE_INSTALL_FILE}
     readarray -t package_names < ${ANDROID_HOME}/${PACKAGE_INSTALL_FILE}
     yes | sdkmanager --verbose --install "${package_names[@]}" | awk -f ${root_dir}/build/awk/reduce.awk
@@ -92,6 +101,7 @@ if [[ ${update_android_sdk} == "1" ]] ; then
 fi
 
 
+echo "docker run and build aar ....."
 GO_PATH_SRC_DIR_IN_DOCKER=/root/go/src/AndroidLibV2rayLite
 docker run --name builder --rm \
     -v /usr/local/go:/opt/go \
